6be2930e710dfee6c896b78a41274aa70c7707b5
Predakor <patrykbusko@gmail.com>
2023-06-01 14:29:21 +0200

    Add: Logo title and description to html head

--- src/components/Task/TaskDetails/Step/StepList.jsx
+++ src/components/Task/TaskDetails/Step/StepList.jsx
@@ -1,17 +1,38 @@
+import { createStep } from "../../../../utils/todos";
+import AddStep from "./AddStep";
 import Step from "./Step";
 import styles from "./Step.module.css";
-function StepList({ steps, editStep, addStep }) {
+
+function StepList({ steps, updateTask }) {
+  const addStep = (newStep) => {
+    const newSteps = [...steps, createStep(newStep)];
+    console.log({ steps, newSteps });
+    updateTask({ steps: newSteps });
+  };
+
+  const editStep = (editedStep) => {
+    const newSteps = steps.map((step) =>
+      step.id === editedStep.id ? editedStep : step
+    );
+    updateTask({ steps: newSteps });
+  };
+
+  const removeStep = (stepID) => {
+    const newSteps = steps.filter((step) => step.id !== stepID);
+    updateTask({ steps: newSteps });
+  };
+
   return (
     <div className={styles.stepList}>
       {steps.map((step) => (
         <Step
-          name={step.name}
-          done={step.completed}
-          onSubmit={editStep}
+          step={step}
+          editStep={editStep}
+          removeStep={removeStep}
           key={step.id}
         />
       ))}
-      <Step onSubmit={addStep} />
+      <AddStep onSubmit={addStep} />
     </div>
   );
 }
